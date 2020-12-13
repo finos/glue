c80eef384722a8585ace4621fdb9c2065ef5493b
Dobromir_Kralev_Epam <Dobromir_Kralev@epam.com>
2020-12-13 14:56:56 +0200

    Add Transaction Relationships page

---
+++ website/src/pages/summary/PropositionOrderTransacition.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import TableList from './TableList';
+import TransactionsTables from './TransactionsTables'
+import styles from '../../css/scss/styles.module.scss';
+
+function PropositionOrderTransactions() {
+    return (
+        <Layout>
+            <h1>Domain Group for Proposal, Orders and Positions</h1>
+            <PropositionOrderTransactionsSummary></PropositionOrderTransactionsSummary>
+            <div className={styles.imgContainer}>
+                <img src="../../../static/img/transactions_relationships_connections.png" />
+            </div>
+            <TableList tablesList={TransactionsTables}></TableList>
+        </Layout>
+    );
+}
+
+const PropositionOrderTransactionsSummary = () => (
+    <div className={styles.summaryContainer}>
+        <p>
+            An Investment proposal is created when a client advisor wants to; modify an existing client's portfolio, suggest new opportunities, simulate trades and investigate the impact upon client holdings. Your service tool should copy the existing (source) portfolio and create a simulated portfolio with the changes. Then compare to the source portfolio.
+        </p>
+        <p>
+            Once the client approves the investment proposition, it generates an order in the related trading system.
+            The portfolio manager may decide to rebalance the portfolio to the approved investment strategy (i.e: may want to buy stock X and sell stock Y).
+        </p>
+    </div >
+);
+
+export default PropositionOrderTransactions;
---
+++ website/src/pages/summary/TransactionsTables.js
@@ -0,0 +1,23 @@
+import {baseUrl} from '../../../docusaurus.config';
+
+const TransactionsTables = [
+    {name:"Fill", link: `${baseUrl}docs/Proposition_Order_Transaction/Fill`},
+    {name:"Financial_Account", link: `${baseUrl}docs/Proposition_Order_Transaction/Financial_Account`},
+    {name:"Financial_Account_Type", link: `${baseUrl}docs/Proposition_Order_Transaction/Financial_Account_Type`},
+    {name:"Investment_Proposition", link: `${baseUrl}docs/Proposition_Order_Transaction/Investment_Proposition`},
+    {name:"Investment_Proposition_Position_Change", link: `${baseUrl}docs/Proposition_Order_Transaction/Investment_Proposition_Position_Change`},
+    {name:"Order", link: `${baseUrl}docs/Proposition_Order_Transaction/Order`},
+    {name:"Order_Completion", link: `${baseUrl}docs/Proposition_Order_Transaction/Order_Completion`},
+    {name:"Portfolio", link: `${baseUrl}docs/Proposition_Order_Transaction/Portfolio`},
+    {name:"Portfolio_Type", link: `${baseUrl}docs/Proposition_Order_Transaction/Portfolio_Type`},
+    {name:"Position", link: `${baseUrl}docs/Proposition_Order_Transaction/Position`},
+    {name:"Transaction_Meta_Type", link: `${baseUrl}docs/Proposition_Order_Transaction/Transaction_Meta_Type`},
+    {name:"Transaction_Relation", link: `${baseUrl}docs/Proposition_Order_Transaction/Transaction_Relation`},
+    {name:"Transaction_Type", link: `${baseUrl}docs/Proposition_Order_Transaction/Transaction_Type`},
+    {name:"Transactions", link: `${baseUrl}docs/Proposition_Order_Transaction/Transactions`},
+    {name:"Currency", link: `${baseUrl}docs/Proposition_Order_Transaction/Currency`},
+    {name:"Currency_Identifier", link: `${baseUrl}docs/Proposition_Order_Transaction/Currency_Identifier`},
+    {name:"Currency_Identifier_Type", link: `${baseUrl}docs/Proposition_Order_Transaction/Currency_Identifier_Type`}
+];
+
+export default TransactionsTables;
